3a43621c8de0d8e79f8437c2fbe0db2a44935d30
Brian Lee <briandl92391@gmail.com>
2017-10-31 07:48:52 -0700

    Added explicit magnet version

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ setup(
         'PyYaml',
         'termcolor',
         'pip',
-        'magnet'
+        'magnet==0.1.1'
     ],
     dependency_links=[
-        "https://github.com/brian-dlee/magnet/archive/master.zip#egg=magnet"
+        "https://github.com/brian-dlee/magnet/archive/master.zip#egg=magnet-0.1.1"
     ],    
     scripts=['scripts/allay'],
     zip_safe=True,
